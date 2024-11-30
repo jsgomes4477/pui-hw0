d32101994bbd8b530c460bafcf5593887ba482ab
jsgomes4477 <jsgomes@andrew.cmu.edu>
2024-11-30 13:46:25 -0500

    getting website ready for resizing w/ Bootstrap
    Perplexity Pro helped with organization here, specifically for the CSS and S files

---
+++ final-project/js/sketch.js
@@ -0,0 +1,24 @@
+let webCanvas, mobileCanvas;
+
+function setup() {
+    // Create canvases that fill their containers
+    webCanvas = createCanvas(windowWidth, windowHeight);
+    webCanvas.parent('web-p5-container');
+
+    mobileCanvas = createCanvas(windowWidth, windowHeight);
+    mobileCanvas.parent('mobile-p5-container');
+
+    windowResized();
+}
+
+function windowResized() {
+    if (windowWidth >= 992) { // Bootstrap's lg breakpoint
+        resizeCanvas(windowWidth, windowHeight);
+        webCanvas.show();
+        mobileCanvas.hide();
+    } else {
+        resizeCanvas(windowWidth, windowHeight);
+        webCanvas.hide();
+        mobileCanvas.show();
+    }
+}
