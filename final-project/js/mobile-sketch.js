642376915359c10aee22041d0012f94091763c5f
jsgomes4477 <jsgomes@andrew.cmu.edu>
2024-11-30 16:18:53 -0500

    roughly finished home page for mobile

--- final-project/js/mobile-sketch.js
+++ final-project/js/mobile-sketch.js
@@ -2,6 +2,8 @@ let inputBox;
 let errorDiv;
 let menuVisible = false;
 let currentColor = '#FFFFFF';
+let arrowHovered = false;
+let mobileCanvas;
 
 function setup() {
     mobileCanvas = createCanvas(430, 932);
@@ -31,6 +33,11 @@ function draw() {
     fill(255);
     rect(2, height-200, width-4, 198);
 
+    // Draw arrow if color is set
+    if (currentColor !== '#FFFFFF') {
+        drawArrow();
+    }
+
     // Draw "HEXCODE:" text
     fill(0);
     textSize(28);
@@ -43,10 +50,10 @@ function draw() {
     text('MENU', 15, height - 30);
 
     if (menuVisible) {
-    textSize(18);
-    text('SWATCHES', 15, height - 60);
-    text('LIBRARY', 15, height - 80);
-    text('HOME', 15, height - 100);
+        textSize(18);
+        text('SWATCHES', 15, height - 60);
+        text('LIBRARY', 15, height - 80);
+        text('HOME', 15, height - 100);
     }
 
     // Draw black border (always do this last)
@@ -56,44 +63,84 @@ function draw() {
     rect(2, 2, width-4, height-4);
 }
 
+function drawArrow() {
+    push();
+    translate(width/2, height-280); // Moved higher above white box
+    stroke(0);
+    strokeWeight(2);
+    fill(arrowHovered ? 150 : 0);
+    
+    // New arrow design
+    beginShape();
+    vertex(-20, 0);    // Left point
+    vertex(0, 20);     // Bottom point
+    vertex(20, 0);     // Right point
+    vertex(10, 0);     // Right inner point
+    vertex(10, -15);   // Right top point
+    vertex(-10, -15);  // Left top point
+    vertex(-10, 0);    // Left inner point
+    endShape(CLOSE);
+    pop();
+}
+
+function mouseWheel(event) {
+    if (currentColor !== '#FFFFFF') {
+        window.location.href = 'library.html';
+        return false;
+    }
+}
+
 function handleInput() {
     let hexValue = inputBox.value();
     let hexRegex = /^#[0-9A-Fa-f]{6}$/;
 
     if (hexRegex.test(hexValue)) {
-    currentColor = hexValue;
-    errorDiv.html('');
-    redraw();
+        currentColor = hexValue;
+        errorDiv.html('');
+        redraw();
     } else if (hexValue.length === 7) {
-    errorDiv.html('Please enter a valid hex code (e.g., #FF0000)');
+        errorDiv.html('Please enter a valid hex code (e.g., #FF0000)');
     }
 }
 
 function mouseMoved() {
+    // Menu hover check
     if (mouseX < 100 && mouseY > height - 100) {
-    menuVisible = true;
+        menuVisible = true;
     } else {
-    menuVisible = false;
+        menuVisible = false;
+    }
+    
+    // Arrow hover check - updated hit area for new arrow
+    if (currentColor !== '#FFFFFF') {
+        let arrowX = width/2;
+        let arrowY = height-280;
+        arrowHovered = (mouseX > arrowX - 20 && mouseX < arrowX + 20 && 
+                       mouseY > arrowY - 15 && mouseY < arrowY + 20);
     }
     redraw();
 }
 
 function mousePressed() {
-    if (menuVisible) {
-    if (mouseY < height - 50 && mouseY > height - 70) {
-        window.location.href = 'swatches.html';
-    } else if (mouseY < height - 70 && mouseY > height - 90) {
+    if (arrowHovered && currentColor !== '#FFFFFF') {
         window.location.href = 'library.html';
-    } else if (mouseY < height - 90 && mouseY > height - 110) {
-        window.location.href = 'index.html';
     }
+    
+    if (menuVisible) {
+        if (mouseY < height - 50 && mouseY > height - 70) {
+            window.location.href = 'swatches.html';
+        } else if (mouseY < height - 70 && mouseY > height - 90) {
+            window.location.href = 'library.html';
+        } else if (mouseY < height - 90 && mouseY > height - 110) {
+            window.location.href = 'index.html';
+        }
     }
 }
 
 function windowResized() {
     resizeCanvas(430, 932);
-    inputBox.position(width/2, height/2);
+    inputBox.position(width/2 - 140, height/2 - 20);
     errorDiv.position(width/2 - 140, height/2 + 30);
 }
 
-// This is p5.js
+// This is done in p5.js, sketch is for the home page of mobile version
